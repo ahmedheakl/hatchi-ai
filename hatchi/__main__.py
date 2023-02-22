631fd54792e4e913d36486d57a7078251f9e3fc5
Ahmed Mohamed Sobhey Heakl <52796111+ahmedheakl@users.noreply.github.com>
2023-02-22 15:05:50 +0200

    Feature - how to contribute
    * Renamed project
    
    * Add how to contribute to docs

---
+++ hatchi/__main__.py
@@ -0,0 +1,17 @@
+"""Main script for Hatchi library"""
+
+
+def main(x_coord: int, y_coord: int) -> int:
+    """Main function for the libray.
+    Should be run as follows:
+
+    ```python
+    if __name__ == "__main__":
+        main()
+    ```
+    """
+    return x_coord + y_coord
+
+
+if __name__ == "__main__":
+    main(2, 3)
