e7f9e040731fc6b0a06fbeb6e8c06497ff0e9430
Ahmed Mohamed Sobhey Heakl <52796111+ahmedheakl@users.noreply.github.com>
2023-02-23 04:02:58 +0200

    Fix: Linters (mypy and pylint)
    * Renamed project
    
    * Add how to contribute to docs
    
    * Added pylint parser for PR comments
    
    * Reverted to old bot
    
    * Reverted to new bot
    
    * Fixed stat data
    
    * Fixing PR bot
    
    * Trying test
    
    * Fixed pytest
    
    * Changed bot name
    
    * Fix distance
    
    * Retained distance'
    
    * Removed bot
    
    * Renamed CI
    
    * Renamed CI title
    
    * Renamed Job
    
    * Fixed linters bots
    
    * Adde mypy linter
    
    * Added linter for mypy
    
    * Fixed style for linters
    
    * Fixing styling issudes
    
    * Fixed lines styling

---
+++ scripts/linters_parser.py
@@ -0,0 +1,61 @@
+"""Collection for stats about code"""
+
+PYLINT_FILE_NAME = "pylint.txt"
+PYLINT_STATUS_NAME = "pylint_status.txt"
+MYPY_FILE_NAME = "mypy.txt"
+MYPY_STATUS_NAME = "mypy_status.txt"
+LINTERS_FILE_NAME = "linters.txt"
+
+
+def get_pylint_data() -> str:
+    """Retrieve pylint data"""
+    pylint_errors = ""
+    with open(PYLINT_FILE_NAME, "r", encoding="utf-8") as pylint_file:
+        for line in pylint_file.readlines():
+            pylint_errors += "\t" + line
+    stat_data = ""
+    with open(PYLINT_STATUS_NAME, "r", encoding="utf-8") as pylint_file:
+        stat_data = pylint_file.readline()
+
+    pylint_status: bool = int(stat_data) == 0
+    if pylint_status:
+        pylint_data = "* Pylint: ran :ok:"
+    else:
+        pylint_data = "* <details><summary>Pylint: problems :warning: (click for details)</summary>\n"
+        pylint_data += f"\n\t```\n{pylint_errors}\t```\n"
+        pylint_data += "</details>"
+    return pylint_data
+
+
+def get_mypy_data() -> None:
+    """Retrieve mypy stats"""
+    mypy_errors = ""
+    with open(MYPY_FILE_NAME, "r", encoding="utf-8") as mypy_file:
+        for line in mypy_file.readlines():
+            mypy_errors += "\t" + line
+    stat_data = ""
+    with open(MYPY_STATUS_NAME, "r", encoding="utf-8") as mypy_file:
+        stat_data = mypy_file.readline()
+
+    mypy_status: bool = int(stat_data) == 0
+    if mypy_status:
+        mypy_data = "* Mypy: ran :ok:"
+    else:
+        mypy_data = "* <details><summary>Mypy: problems :warning: (click for details)</summary>\n"
+        mypy_data += f"\n\t```\n{mypy_errors}\t```\n"
+        mypy_data += "</details>"
+    return mypy_data
+
+
+def main() -> None:
+    """Main script for collecting stats"""
+    linters_data = "#### Linters stats for Hatchi PR\n"
+    linters_data += get_pylint_data() + "\n\n"
+    linters_data += get_mypy_data()
+
+    with open(LINTERS_FILE_NAME, "w", encoding="utf-8") as linters_file:
+        linters_file.write(linters_data)
+
+
+if __name__ == "__main__":
+    main()
