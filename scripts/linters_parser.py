dc7c74cd032c4524ee3a2337f1d06cf7bfb9f2ef
ahmedheakl <ahmed.heakl@ejust.edu.eg>
2023-02-23 03:58:18 +0200

    Fixed lines styling

--- scripts/linters_parser.py
+++ scripts/linters_parser.py
@@ -12,7 +12,7 @@ def get_pylint_data() -> str:
     pylint_errors = ""
     with open(PYLINT_FILE_NAME, "r", encoding="utf-8") as pylint_file:
         for line in pylint_file.readlines():
-            pylint_errors += line
+            pylint_errors += "\t" + line
     stat_data = ""
     with open(PYLINT_STATUS_NAME, "r", encoding="utf-8") as pylint_file:
         stat_data = pylint_file.readline()
@@ -21,10 +21,8 @@ def get_pylint_data() -> str:
     if pylint_status:
         pylint_data = "* Pylint: ran :ok:"
     else:
-        pylint_data = (
-            "<details><summary>Pylint: problems :warning: (click for details)</summary>"
-        )
-        pylint_data += f"```python\n{pylint_errors}\n```"
+        pylint_data = "* <details><summary>Pylint: problems :warning: (click for details)</summary>\n"
+        pylint_data += f"\n\t```\n{pylint_errors}\t```\n"
         pylint_data += "</details>"
     return pylint_data
 
@@ -34,7 +32,7 @@ def get_mypy_data() -> None:
     mypy_errors = ""
     with open(MYPY_FILE_NAME, "r", encoding="utf-8") as mypy_file:
         for line in mypy_file.readlines():
-            mypy_errors += line
+            mypy_errors += "\t" + line
     stat_data = ""
     with open(MYPY_STATUS_NAME, "r", encoding="utf-8") as mypy_file:
         stat_data = mypy_file.readline()
@@ -43,10 +41,8 @@ def get_mypy_data() -> None:
     if mypy_status:
         mypy_data = "* Mypy: ran :ok:"
     else:
-        mypy_data = (
-            "<details><summary>Mypy: problems :warning: (click for details)</summary>"
-        )
-        mypy_data += f"```python\n{mypy_errors}```\n"
+        mypy_data = "* <details><summary>Mypy: problems :warning: (click for details)</summary>\n"
+        mypy_data += f"\n\t```\n{mypy_errors}\t```\n"
         mypy_data += "</details>"
     return mypy_data
 
@@ -54,7 +50,7 @@ def get_mypy_data() -> None:
 def main() -> None:
     """Main script for collecting stats"""
     linters_data = "#### Linters stats for Hatchi PR\n"
-    linters_data += get_pylint_data()
+    linters_data += get_pylint_data() + "\n\n"
     linters_data += get_mypy_data()
 
     with open(LINTERS_FILE_NAME, "w", encoding="utf-8") as linters_file:
