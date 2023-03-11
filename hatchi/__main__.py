e0b5322ffbce5b68ec260e97ac5feb9dcbb8b848
ahmedheakl <ahmed.heakl@ejust.edu.eg>
2023-03-11 02:24:55 +0200

    Added logger module

--- hatchi/__main__.py
+++ hatchi/__main__.py
@@ -1,17 +1,18 @@
 """Main script for Hatchi library"""
+import logging
+
+from utils.general import init_logger
+
+_LOG = logging.getLogger(__name__)
 
 
 def main(x_coord: int, y_coord: int) -> int:
     """Main function for the libray.
     Should be run as follows:
-
-    ```python
-    if __name__ == "__main__":
-        main()
-    ```
     """
     return x_coord + y_coord
 
 
 if __name__ == "__main__":
-    main(2, 3)
+    init_logger()
+    _LOG.info(main(2, 3))
