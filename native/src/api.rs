29395d638ad2db04f8e6d9444c23031de258dfe7
Mohamed Abdeen <mohamedabden21@gmail.com>
2023-03-11 12:53:13 +0200

    print query error message

--- native/src/api.rs
+++ native/src/api.rs
@@ -1,16 +1,20 @@
 use futures::executor::block_on;
-use sqlx::{migrate::MigrateDatabase, Column, Row, SqlitePool};
+use sqlx::{migrate::MigrateDatabase, sqlite::SqlitePoolOptions, Column, Row};
 
 pub fn query_db(uri: String, query: String) -> String {
     block_on(async {
         if !sqlx::Sqlite::database_exists(&uri).await.unwrap() {
             sqlx::Sqlite::create_database(&uri).await.unwrap();
         }
-        let pool = SqlitePool::connect(&uri).await.unwrap();
-        // let query = "DROP TABLE IF EXISTS users;
-        // CREATE TABLE users (id INT, name TEXT);
+        let pool = SqlitePoolOptions::new()
+            .max_connections(1)
+            .connect(&uri)
+            .await
+            .unwrap();
+        // let init = "DROP TABLE IF EXISTS users;
+        // CREATE TABLE IF NOT EXISTS users (id INT, name TEXT);
         // INSERT INTO users VALUES(1,'name'),(2,'hey');";
-        // sqlx::query(&query).execute(&pool).await.unwrap();
+        // sqlx::query(&init).execute(&pool).await.unwrap();
         let rows = sqlx::query(&query).fetch_all(&pool).await;
         let res: String = match rows {
             Ok(rows) => {
@@ -25,7 +29,12 @@ pub fn query_db(uri: String, query: String) -> String {
                 });
                 result
             }
-            Err(_) => String::from("Query returned an error"),
+            Err(err) => {
+                let mut result: String = String::from("Query returned an error: ");
+                let msg: String = format!("{}", err);
+                result.push_str(&msg);
+                result
+            }
         };
         return res;
     })
